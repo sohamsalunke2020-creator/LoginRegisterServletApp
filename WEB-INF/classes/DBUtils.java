d65e56d9f09ff41278dde63cc51093064e46a4c1
Soham Salunke <sohamsalunke2020@gmail.com>
2026-02-13 16:17:52 +0530

    Hide database password in DBUtils.java
    Updated the database password in DBUtils.java to hide sensitive information.

--- WEB-INF/classes/DBUtils.java
+++ WEB-INF/classes/DBUtils.java
@@ -4,6 +4,6 @@ import java.sql.DriverManager;
 public class DBUtils{
     public static Connection establishConnection() throws Exception{
         Class.forName("org.postgresql.Driver");
-        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/userapp","postgres","password");
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/userapp","postgres","*******");
     }
 }
